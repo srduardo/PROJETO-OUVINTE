7c7bd43003926a43c048fa89ed6c9c62cd9e3555
srduardo <eduardodev8@gmail.com>
2025-03-09 16:24:47 -0300

    feat: classe complaint e dtos de request e response criados (dtos ainda vazios)

---
+++ backend/src/main/java/com/ouvinte/backend/dto/request/ComplaintRequestDto.java
@@ -0,0 +1,9 @@
+package com.ouvinte.backend.dto.request;
+
+public class ComplaintRequestDto {
+    private String title;
+    private String description;
+    private String type;
+    private byte[] image;
+
+}
