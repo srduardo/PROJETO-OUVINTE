5fb8ae856e59796a19b48ca366410c85f163e6ec
srduardo <eduardodev8@gmail.com>
2025-04-01 19:07:24 -0300

    feat(ScheduledComplaints): implementa uma service responsável pelo agendamento e deleção das denúncias, considerando o seu tempo de vida (30 dias).

--- backend/src/main/java/com/ouvinte/backend/repositories/ComplaintRepository.java
+++ backend/src/main/java/com/ouvinte/backend/repositories/ComplaintRepository.java
@@ -2,6 +2,7 @@ package com.ouvinte.backend.repositories;
 
 import com.ouvinte.backend.domain.Complaint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
